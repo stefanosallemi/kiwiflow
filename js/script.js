4d8d02b9d718291c7527d345346cdab21f1420f3
stefanosallemi <76653440+stefanosallemi@users.noreply.github.com>
2022-12-19 21:15:01 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4717,23 +4717,23 @@
                 !s &&
                 ((e =
                   e ||
-                  ((n = t('<a class="w-webflow-badge"></a>').attr(
+                  ((n = t('<a "></a>').attr(
                     "href",
-                    "https://webflow.com?utm_campaign=brandjs"
+                    ""
                   )),
                   (r = t("<img>")
                     .attr(
                       "src",
-                      "https://d3e54v103j8qbb.cloudfront.net/img/webflow-badge-icon.f67cd735e3.svg"
+                      ""
                     )
                     .attr("alt", "")
                     .css({ marginRight: "8px", width: "16px" })),
                   (a = t("<img>")
                     .attr(
                       "src",
-                      "https://d1otoma47x30pg.cloudfront.net/img/webflow-badge-text.6faa6a38cd.svg"
+                      ""
                     )
-                    .attr("alt", "Made in Webflow")),
+                    .attr("alt", "")),
                   n.append(r, a),
                   n[0])),
                 d(),
